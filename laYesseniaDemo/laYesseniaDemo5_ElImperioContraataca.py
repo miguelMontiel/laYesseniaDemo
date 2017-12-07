193ed6619741e54f03c1be2d51f0d0ae2cce2236
miguelMontiel <miguelmontielgalicia@hotmail.com>
2017-12-07 13:59:18 -0600

    Add files via upload

--- laYesseniaDemo/laYesseniaDemo5_ElImperioContraataca.py
+++ laYesseniaDemo/laYesseniaDemo5_ElImperioContraataca.py
@@ -2,10 +2,12 @@ import tkinter.filedialog as Tkinter
 import os.path
 import codecs as loscodecs
 
+'''Stubs'''
+
 buscarProgramas = 'setNoServicioCics(obtenerNombreServicio("'
 
-archivos = Tkinter.askopenfilenames(initialdir = "C:/Users/mainUser/Desktop/to/servidor/", title = "Archivos java")
-archivoNuevo = "C:/Users/mainUser/Desktop/nuevos/stubsUsanProgramas.txt"
+archivos = Tkinter.askopenfilenames(initialdir = "C:/Users/IBM_ADMIN/Desktop/INFOPRE/PreINFO-27Abril2016/src/gnp/to/servidor/", title = "Archivos java")
+archivoNuevo = "C:/Users/IBM_ADMIN/Desktop/archivosLaYessenia/stubsUsanProgramas.txt"
 archivoNuevoFinal = open(archivoNuevo, "w+")
 
 print("Por cada Stub que hay aqui, se tienen los programas que se muestras abajo del mismo.", "\n")
@@ -16,13 +18,14 @@ for individuales in archivos:
     nombreArchivo = os.path.basename(str(individuales[:-5]))
 
     if nombreArchivo[:4] != 'Ktol':
-        archivoNuevoFinal.write(nombreArchivo + "\n")
+        #archivoNuevoFinal.write("Stub: " + nombreArchivo + "\n")
         print(nombreArchivo)
         for linea in archivo:
             if buscarProgramas in linea:
                 lineaStrip = linea.strip()
                 print(lineaStrip[41:49])
-                archivoNuevoFinal.write("   " + lineaStrip[41:49] + "\n")
+                archivoNuevoFinal.write("Programa: " + lineaStrip[41:49] + "\n")
+                archivoNuevoFinal.write("Stub: " + nombreArchivo + "\n")
         print("")
         archivoNuevoFinal.write("\n")
     archivo.close()
