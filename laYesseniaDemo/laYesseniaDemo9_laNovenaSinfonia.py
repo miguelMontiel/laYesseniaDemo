210c5f949eb53ce37c83f36e2b18f948616af61a
miguelMontiel <miguelmontielgalicia@hotmail.com>
2017-12-19 13:10:22 -0600

    Add files via upload

--- laYesseniaDemo/laYesseniaDemo9_laNovenaSinfonia.py
+++ laYesseniaDemo/laYesseniaDemo9_laNovenaSinfonia.py
@@ -2,21 +2,25 @@ import tkinter.filedialog as Tkinter
 import os.path
 import codecs as loscodecs
 
-busqueda = input("Que se va a buscareishon: ")
-
 archivos = Tkinter.askopenfilenames(initialdir = "C:/Users/IBM_ADMIN/Desktop/archivosLaYessenia/", title = "Archivos java")
 archivoNuevo = open("programasUsanProgramasPrincipales.txt", "w+")
 
 for individuales in archivos:
     archivo = loscodecs.open(individuales, "r")
     nombreArchivo = os.path.basename(str(individuales[:-5]))
-    print(nombreArchivo, ": ")
+
+    print("------------------------- " + nombreArchivo + " -------------------------")
     archivoNuevo.write("------------------------- " + nombreArchivo + " -------------------------" + "\n")
 
     for linea in archivo:
-        if busqueda in linea:
+        if ".to.cliente" in linea and "package" not in linea:
             print(linea)
             archivoNuevo.write(linea)
+
+        if "ventana" in linea and "= new" in linea:
+            if ".ae." not in linea:
+                print(linea)
+                archivoNuevo.write(linea)
 
     archivoNuevo.write("\n" + "\n")
     print("")
